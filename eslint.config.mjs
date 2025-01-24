92b7221528ffb34d03eda7531d816a655fadeece
lilbigs2001 <joebigelow@protonmail.com>
2025-01-24 15:28:18 -0600

    Set up Jest for project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import pluginJest from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.jest } } },
   pluginJs.configs.recommended,
+  pluginJest.configs.recommended,
   eslintConfigPrettier,
 ];
