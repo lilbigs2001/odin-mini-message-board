d4fe10e9cc8d1ae3e486d0da38bc8bc297970c10
lilbigs2001 <joebigelow@protonmail.com>
2025-01-28 16:41:43 -0600

    Add Submit button to form

--- tests/views/form.ejs.test.js
+++ tests/views/form.ejs.test.js
@@ -12,28 +12,32 @@ afterAll(() => {
   testServer.close();
 });
 
-it("contains DOCTYPE designation", async () => {
+it("contains DOCTYPE designation", () => {
   expect(response.text).toContain("<!DOCTYPE html>");
 });
 
-it("has 'New Message' title", async () => {
+it("has 'New Message' title", () => {
   expect(response.text).toContain("<title>New Message</title>");
 });
 
-it("has a 'New Message' heading", async () => {
+it("has a 'New Message' heading", () => {
   expect(response.text).toContain("<h1>New Message</h1>");
 });
 
-it("has an input for the author's name", async () => {
+it("has an input for the author's name", () => {
   expect(response.text).toContain('<label for="author_name">Username:</label>');
   expect(response.text).toContain(
     '<input type="text" id="author_name" name="username">',
   );
 });
 
-it("has an input for the user message", async () => {
+it("has an input for the user message", () => {
   expect(response.text).toContain('<label for="message">Message:</label>');
   expect(response.text).toContain(
     '<textarea id="message" name="message"></textarea>',
   );
+});
+
+it("has a submit button", () => {
+  expect(response.text).toContain('<button type="submit">Submit</button>');
 });
--- tests/views/index.ejs.test.js
+++ tests/views/index.ejs.test.js
@@ -13,15 +13,15 @@ afterAll(() => {
 });
 
 describe("head partial view", () => {
-  it("has correct encoding", async () => {
+  it("has correct encoding", () => {
     expect(response.text).toContain('<meta charset="UTF-8" />');
   });
 
-  it("has 'Homepage' title", async () => {
+  it("has 'Homepage' title", () => {
     expect(response.text).toContain("<title>Homepage</title>");
   });
 });
 
-it("displays application title", async () => {
+it("displays application title", () => {
   expect(response.text).toContain("<h1>Mini Message Board</h1>");
 });
